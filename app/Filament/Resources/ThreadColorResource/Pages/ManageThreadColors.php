acc65c6849494d4125d0797e82bbec01f0702443
William Hunt <willhunt1994@gmail.com>
2025-10-28 14:29:58 -0700

    Add Download Thread Colors button to Thread Colors page

--- app/Filament/Resources/ThreadColorResource/Pages/ManageThreadColors.php
+++ app/Filament/Resources/ThreadColorResource/Pages/ManageThreadColors.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\ThreadColorResource\Pages;
 use App\Filament\Resources\ThreadColorResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ManageRecords;
+use Filament\Actions\Action;
 
 class ManageThreadColors extends ManageRecords
 {
@@ -13,6 +14,11 @@ class ManageThreadColors extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('downloadThreadColors')
+                ->label('Download Thread Colors')
+                ->icon('heroicon-o-arrow-down-tray')
+                ->url('https://drive.google.com/file/d/1tC7YLVxove4U8sY589lJzf3jqzYXGIsh/view?usp=sharing')
+                ->openUrlInNewTab(),
             Actions\CreateAction::make(),
         ];
     }
